79c0afbfff1aee7f61375704ce5781137fd37b5a
FabianoCarneiro <37809484+FabianoCarneiro@users.noreply.github.com>
2022-02-09 18:17:31 -0300

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -41,21 +41,4 @@ public class DemoApplication {
 	String time(){
 		return new Date().toString();
 	}
-	
-	@GetMapping("/fiap")
-	String turma(){
-		return "DEVOPS carlota
- - VERSAO 1.0";
-
-	}
-
-	
-	@GetMapping("/FeatureFabiano")
-	String fabiano(){
-		return "FeatureFAbiano";
-
-	}
-
-
-
 }	
